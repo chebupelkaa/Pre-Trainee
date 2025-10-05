39fc1a054514964e43f0507fe9d7db8277e8e546
Alina Grin <70023302+chebupelkaa@users.noreply.github.com>
2025-10-06 00:29:33 +0300

    commit to dev
    commit to dev

---
+++ Week1/Task1/Task1/Calculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    public class Calculator
+    {
+        static void Main(string[] args)
+        {
+            bool continueInput = true;
+
+            while (continueInput)
+            {
+                try
+                {
+                    double firstNumber = GetNumber("Введите первое число: ");
+
+                    char operation = GetOperation();
+
+                    double secondNumber = GetNumber("Введите второе число: ");
+
+                    double result = DoOperation(firstNumber, secondNumber, operation);
+
+                    Console.WriteLine($"Ответ: {firstNumber} {operation} {secondNumber} = {result}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Ошибка: {e.Message}");
+                }
+
+                continueInput = AskToContinue();
+            }
+
+        }
+        public static double DoOperation(double first, double second, char operation)
+        {
+            switch (operation)
+            {
+                case '+': return first + second;
+                case '-': return first - second;
+                case '*': return first * second;
+                case '/':
+                    if (second == 0)
+                    {
+                        throw new DivideByZeroException("Деление на ноль невозможно");
+                    }
+                    return first / second;
+                default: throw new ArgumentException("Неизвестная операция");
+            }
+        }
+
+        public static double GetNumber(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                string input = Console.ReadLine();
+
+                if (double.TryParse(input, out double number))
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: пожалуйста, введите число правильно");
+                }
+            }
+        }
+
+        public static char GetOperation()
+        {
+            while (true)
+            {
+                Console.Write("Выберите операцию (+, -, *, /):");
+                string inputOperation = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(inputOperation) && "+-*/".Contains(inputOperation[0]))
+                {
+                    return inputOperation[0];
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: выберите одну из следующих операций: +, -, *, /");
+                }
+            }
+        }
+
+        public static bool AskToContinue()
+        {
+            while (true)
+            {
+                Console.Write("Хотите ли вы провести еще одну операцию? (y/n):");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "y")
+                {
+                    return true;
+                }
+                else if (input == "n")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Введите «y» для продолжения или «n» для выхода.");
+                }
+            }
+        }
+    }
+}
---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var syncStopwatch = Stopwatch.StartNew();
+
+            string result1 = ProcessData("Файл 1");
+            Console.WriteLine(result1);
+
+            string result2 = ProcessData("Файл 2");
+            Console.WriteLine(result2);
+
+            string result3 = ProcessData("Файл 3");
+            Console.WriteLine(result3);
+
+            syncStopwatch.Stop();
+            Console.WriteLine($"Общее время синхронной обработки: {syncStopwatch.Elapsed.TotalSeconds:F2} секунд\n");
+
+            var asyncStopwatch = Stopwatch.StartNew();
+
+            var firstDataSet = new List<string> { "Файл 1", "Файл 2", "Файл 3" };
+            RunAsyncProcessing(firstDataSet).Wait();
+
+            asyncStopwatch.Stop();
+            Console.WriteLine($"Общее время асинхронной обработки: {asyncStopwatch.Elapsed.TotalSeconds:F2} секунд");
+
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+        static string ProcessData(string dataName)
+        {
+            Console.WriteLine($"Начало обработки {dataName}...");
+            Thread.Sleep(3000);
+            return $"Обработка '{dataName}' завершена за 3 секунды";
+        }
+
+        static async Task<string> ProcessDataAsync(string dataName)
+        {
+            Console.WriteLine($"Начало асинхронной обработки {dataName}...");
+            await Task.Delay(3000); 
+            return $"Обработка '{dataName}' завершена за 3 секунды ";
+        }
+
+        static async Task RunAsyncProcessing(IEnumerable<string> dataSet)
+        {
+            var tasks = new List<Task<string>>();
+
+            foreach (var dataName in dataSet)
+            {
+                tasks.Add(ProcessDataAsync(dataName));
+            }
+
+            Console.WriteLine("Ожидание завершения по мере готовности:\n");
+
+            var remainingTasks = new List<Task<string>>(tasks);
+
+            while (remainingTasks.Count > 0)
+            {
+                Task<string> completedTask = await Task.WhenAny(remainingTasks);
+                remainingTasks.Remove(completedTask);
+                string result = await completedTask;
+                Console.WriteLine($"{result}");
+            }
+        }
+    }
+}
